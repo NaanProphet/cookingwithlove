6a2e33a97690fe6c309780870c93d4ce34516a42
Krishna Bhamidipati <krishna.bhamidipati@accenturefederal.com>
2020-09-19 17:14:17 -0400

    basic config for cooking with love

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+   plugins: [
+    {
+      resolve: "@marinda/gatsby-theme-recipes",
+      options: {
+        // your site title
+        title: "Cooking with Love",
+        // your site short title
+        shortTitle: "Learning how to make Fooz",
+        // the path to your icon file
+        iconPath: "./src/images/favicon.png"
+      }
+    }
+  ]
+};
